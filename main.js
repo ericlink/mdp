9e60e13635c1e39c3fa74007c4ee74e96ae9de3a
Eric Link <elink@examsoft.com>
2018-11-17 16:11:12 -0600

    init

---
+++ main.js
@@ -0,0 +1,37 @@
+// update name, productName, description and config.app.url in package.json
+const { app, BrowserWindow, session } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+const cookie = require('./cookie.js');
+const menu   = require('./menu.js');
+const pjson  = require('./package.json');
+
+let window = null;
+
+app.on('ready', () => {
+  menu.setupMenu();
+  cookie.initCookieManager(session.defaultSession);
+  createMainWindow();
+  window.loadURL(pjson.config.app.url);
+  window.once('ready-to-show', () => {
+    window.show();
+  });
+});
+
+function createMainWindow() {
+  let mainWindowState = windowStateKeeper({ defaultWidth: 1000, defaultHeight: 800 });
+
+  window = new BrowserWindow({
+    show: false,
+    title: pjson.name,
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
+    webPreferences: { 
+      nodeIntegration: false,
+      partition: "persist:main" 
+    }
+  });
+
+  mainWindowState.manage(window);
+}
---
+++ menu.js
@@ -0,0 +1,34 @@
+const { Menu } = require('electron');
+
+exports.setupMenu = function() {
+    const template = [
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' }
+      ]
+    },
+    {
+      label: 'Actions',
+      submenu: [
+        { role: 'quit' },
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' }
+      ]
+    }
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+};

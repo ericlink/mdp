a0d41093e742ee763a85fed9519c3992bb9a08d2
Eric Link <elink@examsoft.com>
2019-03-18 21:03:29 -0500

    open file and logging working
    electron-log, single instance on mac, working from finder, multiple from command line.  need to consolidate file open vars and refactor dupe code.

--- src/main.js
+++ src/main.js
@@ -4,46 +4,90 @@ const menu   = require('./menu.js');
 const path = require('path');
 const update = require('update-electron-app');
 const url = require('url');
-
+const log = require('electron-log');
 
 let window = null;
 
 app.on('open-file', function(event, filePath){
   event.preventDefault();
+  log.info('open-file',event,filePath);
+  global.openFile = {filePath: filePath};
+
+  //////////
+  createMainWindow();
+  window.loadURL('file://' + path.join(__dirname, './renderer/index.html'));
+  window.once('ready-to-show', () => { window.show(); });
+  //////////
 });
 
-app.on('ready', () => {
-  createMainWindow();
-  menu.setupMenu(app);
 
-  global.sharedObject = {argv: process.argv}
+const gotTheLock = app.requestSingleInstanceLock()
 
-  window.loadURL('file://' + path.join(__dirname, './renderer/index.html'));
-  /*
-  window.loadURL(url.format({
-    pathname: path.join(__dirname, 'view/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-  */
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (window) {
+      if (window.isMinimized()) window.restore();
+      window.focus();
+    }
+    log.info('second-instance', event, commandLine, workingDirectory);
+    // create another window with the new doc
+    createMainWindow();
+    menu.setupMenu(app);
 
-  window.once('ready-to-show', () => {
-    window.show();
+    global.sharedObject = {commandLine: commandLine};
+    window.loadURL('file://' + path.join(__dirname, './renderer/index.html'));
+    /*
+      window.loadURL(url.format({
+        pathname: path.join(__dirname, 'view/index.html'),
+        protocol: 'file:',
+        slashes: true
+      }));
+      */
+
+    window.once('ready-to-show', () => {
+      window.show();
+    });
+
+
   });
 
-});
+  app.on('ready', () => {
+    createMainWindow();
+    menu.setupMenu(app);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-});
+    global.sharedObject = {argv: process.argv}
+    window.loadURL('file://' + path.join(__dirname, './renderer/index.html'));
+    /*
+      window.loadURL(url.format({
+        pathname: path.join(__dirname, 'view/index.html'),
+        protocol: 'file:',
+        slashes: true
+      }));
+      */
 
+    window.once('ready-to-show', () => {
+      window.show();
+    });
+
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  });
+
+  /*
 app.on('activate', () => {
   if (window === null) {
     createWindow()
   }
 });
+*/
+
+}
 
 
 function createMainWindow() {
--- src/renderer/script.js
+++ src/renderer/script.js
@@ -4,6 +4,7 @@ const hljs = require('highlight.js')
 const marked = require('marked')
 const mermaid = require('mermaid')
 const remote = require('electron').remote
+const log = require('electron-log');
 
 var mermaidConfig = {
   startOnLoad:false,
@@ -37,8 +38,21 @@ const readFile = (file) => {
 
 // 2nd arg for dev, first arg for normal, README.md as default
 const argv = remote.getGlobal('sharedObject').argv;
-const path = argv[1] === './src/main.js' ? argv[2] : argv[1];
-const file = path || 'README.md';
+const commandLine = remote.getGlobal('sharedObject').commandLine;
+const secondInstanceFile = commandLine && commandLine[3] ? commandLine[3] : null;
+const openFileFilePath = remote.getGlobal('openFile') ?  remote.getGlobal('openFile').filePath : null;
+log.info('argv', argv);
+log.info('commandLine', commandLine);
+log.info('secondInstanceFile', secondInstanceFile);
+log.info('openFileFilePath', openFileFilePath);
+
+let path = null;
+if (argv) {
+  path = argv[1] === './src/main.js' ? argv[2] : argv[1];
+}
+
+const file = openFileFilePath || secondInstanceFile || path || 'README.md';
+log.info('file',file);
 readFile(file);
 
 const watcher = chokidar.watch(file, { ignored: /[\/\\]\./, persistent: true });

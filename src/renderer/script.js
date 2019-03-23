4dee3a95e459dfe0ff906ed2ed21ffffaaa09072
Eric Link <elink@examsoft.com>
2019-03-22 19:49:45 -0500

    prepare to move selected file logic to main, out of renderer

--- src/renderer/script.js
+++ src/renderer/script.js
@@ -37,22 +37,30 @@ const readFile = (file) => {
 }
 
 // 2nd arg for dev, first arg for normal, README.md as default
-const argv = remote.getGlobal('sharedObject').argv;
 const commandLine = remote.getGlobal('sharedObject').commandLine;
 const secondInstanceFile = commandLine && commandLine[3] ? commandLine[3] : null;
 const openFileFilePath = remote.getGlobal('openFile') ?  remote.getGlobal('openFile').filePath : null;
-log.info('argv', argv);
+
+let path = null;
+
+const argv = remote.getGlobal('sharedObject').argv;
+if (argv && argv.length > 1 && argv[1] !== './src/main.js') {
+  log.info('argv', argv);
+  log.info('argv len=1', argv[argv.length -1]);
+  path = argv[argv.length -1];
+}
+
 log.info('commandLine', commandLine);
 log.info('secondInstanceFile', secondInstanceFile);
 log.info('openFileFilePath', openFileFilePath);
+log.info('path', path);
 
-let path = null;
-if (argv) {
-  path = argv[1] === './src/main.js' ? argv[2] : argv[1];
-}
 
+// assign from shared and clear shared
+// move all logic to main to set single shared
 const file = openFileFilePath || secondInstanceFile || path || 'README.md';
 log.info('file',file);
+
 readFile(file);
 
 const watcher = chokidar.watch(file, { ignored: /[\/\\]\./, persistent: true });
